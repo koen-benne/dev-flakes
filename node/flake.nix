bf02c690b0b2b6548b68db18c6dac3b477fbdfbf
koen-benne <koen.benne@gmail.com>
2024-08-01 10:59:54 +0200

    update

---
+++ node/flake.nix
@@ -0,0 +1,33 @@
+# Dream2nix does not seem like it works very well with non-local flakes
+{
+  description = "Flake for node projects";
+
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            (yarn.override { nodejs = pkgs.nodejs; })
+            (pnpm.override { nodejs = pkgs.nodejs; })
+          ];
+        };
+      };
+    };
+}
---
+++ node16/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake for node16 projects";
+
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/nixos-23.*.tar.gz";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            (yarn.override { nodejs = pkgs.nodejs; })
+            (pnpm.override { nodejs = pkgs.nodejs; })
+          ];
+        };
+      };
+    };
+}
