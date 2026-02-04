33bf764ca3fdb337a5c4b798da5c548c56c85d28
Koen Benne <koen.benne@gmail.com>
2026-02-04 16:37:18 +0100

    add another flake for 3.0

---
+++ ruby-3-0-rails/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    nixpkgs-ruby,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [nixpkgs-ruby.overlays.default];
+      };
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            # Node.js and Yarn
+            nodejs_20
+            yarn
+
+            "ruby-3.0.5"
+            bundler
+            docker
+            postgresql_17
+            libyaml # NOTE: for psych gem
+          ];
+        };
+    });
+}
+
